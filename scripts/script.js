aff6038a59f748c7e57ca1e5edef642c228355de
JasonSatherr <jsather@ucsd.edu>
2021-05-15 00:17:00 -0700

    DID PART 1A BABY!

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,51 @@ import { router } from './router.js'; // Router imported so you can use it to ma
 const setState = router.setState;
 
 // Make sure you register your service worker here too
-
+let entryNumber = 0;
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
     .then(entries => {
       entries.forEach(entry => {
+        entryNumber +=1;
         let newPost = document.createElement('journal-entry');
+        newPost.number = entryNumber;
         newPost.entry = entry;
         document.querySelector('main').appendChild(newPost);
+        newPost.addEventListener('click', event => {
+          let pageObj = {
+            page:"single",
+            numero: 0,
+            pageData: {}
+          };
+          pageObj.numero = newPost.number;
+          pageObj.pageData = newPost.entry;
+          history.pushState(pageObj, "single", "#single" + newPost.number)
+          router.setState("single", newPost.number, newPost.entry);
+          
+        });
       });
     });
 });
+
+
+let settings = document.querySelector('header > img');
+settings.addEventListener('click', event => {
+  history.pushState({page: "settings"}, "settings", "#settings");
+  router.setState("settings");
+});
+
+
+let headerText = document.querySelector('header > h1');
+headerText.addEventListener('click', event => {
+  history.pushState({page: "default"}, "default", "#default");
+  router.setState("default");
+});
+
+
+
+// entries.addEventListener('click', event => {
+//   alert("hey");
+//   //router.setState("poggers");
+  
+// });
